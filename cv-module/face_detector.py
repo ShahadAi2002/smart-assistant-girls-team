caf0330fa70e020321e773c43580eb9cfe8d4d99
unknown <rahafalhrbi85@gmail.com>
2026-06-23 22:27:09 +0300

    Implement draw_faces function

--- cv-module/face_detector.py
+++ cv-module/face_detector.py
@@ -10,10 +10,18 @@ def detect_faces(image_path):
     
     face = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=7, minSize=(50, 50))
 
-    #drawing bounding boxes
-    for (x, y, w, h) in face:
-       cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 0), 2) 
-
     return len(face), face   
 
+def draw_faces(image_path, output_path):
+   # read the image
+   img = cv2.imread(image_path)
+   
+   #call detect_faces
+   __,face = detect_faces(image_path)
 
+   #drawing bounding boxes
+   for (x, y, w, h) in face:
+       cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 0), 2) 
+   
+   #save img
+   cv2.imwrite(output_path, img)    
